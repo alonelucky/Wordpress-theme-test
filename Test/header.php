2dc540cd6b2708a0f76ee872fa17c3c2f2b2f4ba
Xiao <xiaqiubo123@163.com>
2016-08-06 11:58:03 +0800

    增加头部文件注释

--- Test/header.php
+++ Test/header.php
@@ -1,9 +1,9 @@
 <!doctype html>
 <html>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>"><!-- 网页文字编码 -->
 	<meta name="viewport" content="width=device-width">
-	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>"><!-- 外部样式表链接 -->
 </head>
 <body>
 	
